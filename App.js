82eb854ea03b108d202c19c96e885d63c90dd90f
vm1234-code <vmachhi@DESKTOP-72MPA42>
2023-06-06 10:10:21 -0700

    Migrating GoalInput component within GoalInput.js. This also adds functionality of the component to remove the text from the box after clicking add goal

--- App.js
+++ App.js
@@ -1,33 +1,28 @@
-import { StyleSheet, View, Button, TextInput, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 
 import { useState } from 'react';
 
 import GoalItem from './components/GoalItem';
-
+import GoalInput from './components/GoalInput';
 
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
+  
 
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText);
-  }
 
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText) {
     setCourseGoals(currentCourseGoals => [
       ...courseGoals, 
       {text: enteredGoalText, key: Math.random().toString() },
     ]);
   }
   
+ 
+
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' 
-        onChangeText={goalInputHandler}/>
-        <Button title='Add Goal' onPress={addGoalHandler}/>
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList 
         data={courseGoals} 
@@ -47,22 +42,7 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal:16
   },
-  inputContainer: {
-    flex:1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: 'grey'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding:8
-  },
+
   goalsContainer: {
     flex: 5
   },
