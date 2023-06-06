678fc390a34ceb3d0c9a44e15904fd677872f3cf
vm1234-code <vmachhi@DESKTOP-72MPA42>
2023-06-06 10:51:50 -0700

    Adding functionality to highlight/make opacity when the button for removal is clicked

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,11 +3,14 @@ import {StyleSheet, View, Text, Pressable} from 'react-native';
 function GoalItem(props) {
 
     return(     
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-             <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{props.text}</Text>
-            </View>
+        <View style={styles.goalItem}>
+        <Pressable 
+            onPress={props.onDeleteItem.bind(this, props.id)}
+            style={({pressed}) => pressed && styles.pressedItem}>
+            <Text style={styles.goalText}>{props.text}</Text>
+            
         </Pressable>
+        </View>
     );
 }
 
@@ -20,7 +23,10 @@ const styles = StyleSheet.create({
         borderRadius: 6,
         backgroundColor: '#5e0acc',
         color: 'white'
-        
+      },
+      pressedItem: {
+        opacity: 0.5
+
       },
       goalText: {
         color: 'white'
